a14be4fbb120a66cb971d7997548af37ce827c72
QuanLew <quan,le01@sjsu.edu>
2025-09-19 23:34:54 -0700

    add name repository in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ["**/*.glb"],
-  base: '/YOUR_REPOSITORY_NAME/'
+  base: "/quan-portfolio/",
 });
